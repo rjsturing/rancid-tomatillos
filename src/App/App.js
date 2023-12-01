3b3c30adc200f7ffc902504834979e44a647d93b
Erica Hagle <ericahagle.dev@gmail.com>
2023-11-30 20:33:18 -0800

    feat: Display header title via App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,17 @@
 import './App.css';
 import movieData from '../mock-data';
+import { useState } from 'react';
 
 function App() {
+  const [movies, setMovies] = useState([]);
 
+  return (
+    <main className='App'>
+      <header>
+        <h1>Rancid Tomatillos</h1>
+      </header>
+    </main>
+  )
 }
 
 export default App;
