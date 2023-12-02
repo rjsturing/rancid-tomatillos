ae9b173eb1fb9413cc884824d022573d567ef180
Bobby Steckline <rjsturing@gmail.com>
2023-12-02 13:08:12 -0500

    Add transparent background

--- src/App/App.js
+++ src/App/App.js
@@ -1,38 +1,40 @@
-import './App.css';
-import Movies from '../Movies/Movies';
-import MovieDetail from '../MovieDetail/MovieDetail';
-import Card from '../Card/Card';
-import movieData from '../mock-data';
-import { useState, useEffect } from 'react';
-
+import "./App.css";
+import Movies from "../Movies/Movies";
+import MovieDetail from "../MovieDetail/MovieDetail";
+import Card from "../Card/Card";
+import movieData from "../mock-data";
+import { useState, useEffect } from "react";
 
 function App() {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
     if (selectedMovie) {
-      document.body.style.background = `rgba(0, 0, 0, 0.5) url(${selectedMovie.backdrop_path}) no-repeat center center fixed`; 
+      document.body.style.background = `linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url(${selectedMovie.backdrop_path}) no-repeat center center fixed`; 
       document.body.style.backgroundSize = 'cover';
     } else {
       document.body.style.background = '';
     }
-  }, [selectedMovie])
+  }, [selectedMovie]);
 
   const selectMovie = (movie) => {
     setSelectedMovie(movie);
-  }
+  };
 
   const clearMovieSelection = () => {
     setSelectedMovie(null);
-  }
+  };
 
   return (
-    <main className='App'>
+    <main className="App">
       <header>
         <h1>Rancid Tomatillos</h1>
       </header>
       {selectedMovie ? (
-        <MovieDetail movie={selectedMovie} clearMovieSelection={clearMovieSelection} />
+        <MovieDetail
+          movie={selectedMovie}
+          clearMovieSelection={clearMovieSelection}
+        />
       ) : (
         <Movies movies={movieData.movies} selectMovie={selectMovie} />
       )}
