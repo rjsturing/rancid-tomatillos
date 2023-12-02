a23d167cd6148e0fdc952f1603abd9998c1308b8
Bobby Steckline <rjsturing@gmail.com>
2023-12-01 21:21:11 -0500

    Round up movie rating percentage and add back button without functionality

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './MovieDetail.css';
 
-function MovieDetail({ movie }) {
+function MovieDetail({ movie, clearMovie}) {
     return (
       <div className="movie-detail">
-        <img className="backdrop" src={movie.backdrop_path} alt={`${movie.title} backdrop`} />
         <img className="poster" src={movie.poster_path} alt={`${movie.title} poster`} />
+        <img className="backdrop" src={movie.backdrop_path} alt={`${movie.title} backdrop`} />
         <h2>{movie.title}</h2>
         <p>Release Date: {movie.release_date}</p>
-        <p>Average Rating: {movie.average_rating}</p>
+        <p>Average Rating: {Math.round(movie.average_rating * 10)}%</p>
+        <button onClick={clearMovie} className="back-button">Back to Movies</button>
       </div>
     );
   }
