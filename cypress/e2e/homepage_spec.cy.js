1c38114f54e4cbb3852cd7ab9250e7fce89ebf46
Bobby Steckline <rjsturing@gmail.com>
2023-12-05 10:10:38 -0500

    Remove CSS changes from Cypress branch

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,9 +1,5 @@
 describe("Rotten Tomatillos", () => {
 
-  it('Should successfully load application when application is opened', () => {
-    cy.visit('/')
-  });
-
   it("Should be able to view home page and movies for selection", () => {
     cy.visit('/')
   });
