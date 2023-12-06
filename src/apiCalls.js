af4d695a079ffd3d365c00a963f2ecdf13820308
Erica Hagle <ericahagle.dev@gmail.com>
2023-12-05 21:51:09 -0800

    feat: Add fetch call for selected movie details

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -8,4 +8,14 @@ const getAllMovies = () => {
     })
 };
 
-export { getAllMovies };
+const getSelectedMovie = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not OK`);
+      }
+      return response.json();
+    })
+}
+
+export { getAllMovies, getSelectedMovie };
