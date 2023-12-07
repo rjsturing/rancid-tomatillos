deff147a9e292a10664e7c83305f7c26ede0d744
Erica Hagle <ericahagle.dev@gmail.com>
2023-12-07 15:25:06 -0800

    test: Add test for main page header

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -1,7 +1,14 @@
 describe("Rotten Tomatillos", () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {
+      statusCode: 200,
+      fixture: '/mock-data.json'
+    });
+    cy.visit('/');
+  });
 
-  it("Should be able to view home page and movies for selection", () => {
-    cy.visit('/')
+  it("should be able to see the title of the page in the header", () => {
+    cy.get('h1').contains('Rancid Tomatillos');
   });
 
 });
