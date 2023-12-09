c66dc65084e2302bffa61236819ea758c0dff3b2
Bobby Steckline <rjsturing@gmail.com>
2023-12-09 15:00:58 -0500

    Add title prop for impaired users

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,9 +1,9 @@
 import './Card.css';
 
-function Card({ poster, onClick }) {
+function Card({ poster, title, onClick }) {
   return (
     <div className='card' onClick={onClick}>
-      <img className='movie-poster' src={poster} alt="Movie Poster" />
+      <img className='movie-poster' src={poster} alt={`Movie Poster for ${title}`} />
     </div>
   );
 }
