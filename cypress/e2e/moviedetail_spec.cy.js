cc83907a5cf6099ad960cc43fe7e9399331b27b6
Bobby Steckline <rjsturing@gmail.com>
2023-12-10 16:02:03 -0500

    Get mock data working

--- cypress/e2e/moviedetail_spec.cy.js
+++ cypress/e2e/moviedetail_spec.cy.js
@@ -1,14 +1,31 @@
 describe("Movie Details", () => {
-  const firstMovieId = "436270";
+  const firstMovieId = "694919";
   const lastMovieId = "585244";
 
   beforeEach(() => {
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        statusCode: 200,
+        fixture: "/mock-data.json",
+      }
+    );
     cy.intercept(
       "GET",
       `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${firstMovieId}`,
       {
         statusCode: 200,
         fixture: "/mock-movie-0.json",
+      }
+    );
+
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        statusCode: 200,
+        fixture: "/mock-data.json",
       }
     );
 
@@ -24,19 +41,19 @@ describe("Movie Details", () => {
     cy.visit("/");
   });
 
+  //it block for back button
+  //pick the first movie - click for first movie, get the button, confirm that it says back or arrow, confirm url goes back to main
+
   it("should navigate to the correct detail page when the first movie card is clicked", () => {
     cy.get(".movies-container").children().first().click();
     cy.url().should("include", `/movies/${firstMovieId}`);
+    //header contains RT
+    //title contains ""
   });
 
-  it("should be able to see the details of the first movie", () => {
-    cy.get(".movies-container").children().first().click();
-    cy.url().should("include", `/movies/${firstMovieId}`);
+  it("should navigate to the correct detail page when the last movie card is clicked", () => {
+    cy.get(".movies-container").children().last().click();
+    cy.url().should("include", `/movies/${lastMovieId}`);
   });
-
-  // it("should navigate to the correct detail page when the last movie card is clicked", () => {
-  //   cy.get(".movies-container").children().last().click();
-  //   cy.url().should("include", `/movies/${lastMovieId}`);
-  // });
 
 });
