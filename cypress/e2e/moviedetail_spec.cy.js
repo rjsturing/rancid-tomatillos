ccc97c729d6c4c1cf4b1038d73d9342c81107dba
Erica Hagle <ericahagle.dev@gmail.com>
2023-12-10 19:50:50 -0800

    test: Add detail page test for last movie

--- cypress/e2e/moviedetail_spec.cy.js
+++ cypress/e2e/moviedetail_spec.cy.js
@@ -47,6 +47,20 @@ describe("Movie Details", () => {
     cy.get(".synopsis-container").find("p").contains("A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.");
   });
 
+  it("should navigate to the correct detail page when the last movie card is clicked, and display details about the chosen movie", () => {
+    cy.get(".movies-container").children().last().click();
+    cy.url().should("include", "/movies/585244");
+    cy.get("img").should("have.attr", "alt", "Tomatillo Logo");
+    cy.get("h1").contains("Rancid Tomatillos");
+    cy.get(".poster").should("have.attr", "alt", "I Still Believe poster");
+    cy.get(".right-side").contains("h3", "I Still Believe (2020)");
+    cy.get(".right-side").contains("h2", "One love can change your life.");
+    cy.get(".right-side").contains("p", "Music, Drama, Romance • 115 mins");
+    cy.get(".right-side").find("img").should("have.attr", "alt", "Tomatillo");
+    cy.get(".right-side").find("b").contains("3.3");
+    cy.get(".synopsis-container").find("p").contains("The true-life story of Christian music star Jeremy Camp and his journey of love and loss that looks to prove there is always hope.");
+  });
+
   // it("should navigate to the correct detail page when the last movie card is clicked", () => {
   //   cy.get(".movies-container").children().last().click();
   //   cy.url().should("include", `/movies/${lastMovieId}`);
