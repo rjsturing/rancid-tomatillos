21de1ce59b5255a7c620ae1db593ab5d6b1d795e
Erica Hagle <ericahagle.dev@gmail.com>
2023-12-10 16:56:05 -0800

    fix: Update main page Cypress tests

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -8,11 +8,20 @@ describe("Rotten Tomatillos", () => {
   });
 
   it("should be able to see the title of the page in the header", () => {
+    cy.get('img').should('have.attr', 'alt', 'Tomatillo Logo');
     cy.get('h1').contains('Rancid Tomatillos');
   });
 
   it('should be able to see the movies populate on the page', () => {
-    cy.get('.movies-container').children().first().find('.movie-poster').should('have.attr', 'alt', 'Money Plane');
-    cy.get('.movies-container').children().last().find('.movie-poster').should('have.attr', 'alt', 'I Still Believe');
+    cy.get('.movies-container')
+      .children()
+      .first()
+      .find('.movie-poster')
+      .should('have.attr', 'alt', 'Movie Poster for Money Plane');
+    cy.get('.movies-container')
+      .children()
+      .last()
+      .find('.movie-poster')
+      .should('have.attr', 'alt', 'Movie Poster for I Still Believe');
   });
 });
